214227021dc49aa4617292c8f6277fb75bf43127
LarsHaack <lars.haack@gmx.de>
2020-10-13 16:09:21 +0200

    Changes for compilation in cygwin environment. Replaces existing functionality with system_error.

--- lib/Socket.cpp
+++ lib/Socket.cpp
@@ -233,6 +233,16 @@ void UDPSocket::send(const std::vector<uint8_t>& data, InetAddress destination)
 
 void UDPSocket::joinGroup(const char* groupname, const char* if_addr)
 {
+#ifdef CYGWIN_BUILD	
+    throw runtime_error(string("UDPSocket::joinGroup()  NOT SUPPORTED IN CYGWIN BUILD"));
+#else
+    struct ip_mreqn {
+               struct in_addr imr_multiaddr; /* IP multicast group
+                                                address */
+               struct in_addr imr_address;   /* IP address of local
+                                                interface */
+               int            imr_ifindex;   /* interface index */
+           };
     ip_mreqn group;
     if ((group.imr_multiaddr.s_addr = inet_addr(groupname)) == INADDR_NONE) {
         throw runtime_error("Cannot convert multicast group name");
@@ -252,6 +262,7 @@ void UDPSocket::joinGroup(const char* groupname, const char* if_addr)
             == SOCKET_ERROR) {
         throw runtime_error(string("Can't join multicast group") + strerror(errno));
     }
+#endif	
 }
 
 void UDPSocket::setMulticastSource(const char* source_addr)
